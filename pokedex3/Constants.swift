bd5715859e949b6197b0cf94ad7b6c1f578b13b9
Edward Barber <edwardbar423.developer@gmail.com>
2017-02-22 16:08:03 -0500

    Finished - Will have to remember to add more commits next time to archive progress.

---
+++ pokedex3/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex3
+//
+//  Created by Edward Barber on 2/22/17.
+//  Copyright © 2017 Edward Barber. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
